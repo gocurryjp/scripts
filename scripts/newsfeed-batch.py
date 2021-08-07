cb9f44b5687364abc24b3705425dbb4153bc979b
Arghya Guha <guha.agt@gmail.com>
2021-08-08 04:16:05 +0900

    fix charset encoding issue

--- scripts/newsfeed-batch.py
+++ scripts/newsfeed-batch.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python
 
-# -*- coding: utf-8 -*-
-
 from facebook_scraper import *
 import sqlalchemy as db
 
 # DB config
-URL='localhost'
-DB='gocurry'
-USR='root'
-PWD='password'
+URL = 'localhost'
+DB = 'gocurry'
+USR = 'user'
+PWD = 'password'
 
-SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}:3306/{}'.format(USR, PWD,URL,DB)
+SQLALCHEMY_DATABASE_URI = 'mysql://{}:{}@{}:3306/{}?charset=utf8mb4'.format(
+    USR, PWD, URL, DB)
 
 # Create connection to DB
-engine = db.create_engine(SQLALCHEMY_DATABASE_URI) 
+engine = db.create_engine(SQLALCHEMY_DATABASE_URI)
 connection = engine.connect()
 
 # we will fetch data from gocurry's facebook page but also select restaurants with their permission
@@ -24,8 +23,7 @@ pages_list = [
 ]
 
 for page in pages_list:
-    for post in get_posts(page, pages=1, cookies="cookies.txt", encoding='utf-8'):
-        
+    for post in get_posts(page, pages=1, cookies="/root/gocurry/cookies.txt"):
         if(page == 'gocurryjp'):
             isGoCurryPage = 1
         else:
@@ -34,14 +32,13 @@ for page in pages_list:
         query = "INSERT IGNORE INTO  `news_feed` (`post_id`, `post_text`, `post_url`, `username`, `time`, `is_gocurry_page`, `source`, `likes`) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
 
         data = (
-            post['post_id'], 
-            post['post_text'].encode('utf8'), 
-            post['post_url'], 
-            post['username'], 
-            post['time'], 
+            post['post_id'],
+            str(post['post_text']),
+            post['post_url'],
+            str(post['username']),
+            post['time'],
             isGoCurryPage,
             'facebook',
             post['likes']
-        )    
-
-        ResultProxy = connection.execute(query,data)
+        )
+        ResultProxy = connection.execute(query, data)
